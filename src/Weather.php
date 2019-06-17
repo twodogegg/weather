489533d613ac2f40e75290c4be135dafc76c6875
dogegg <shuliuzhenhua@gmail.com>
2019-06-17 23:13:23 +0800

    解决冲突

--- src/Weather.php
+++ src/Weather.php
@@ -77,10 +77,8 @@ class Weather
      * @throws HttpException
      * @throws InvalidArgumentException
      */
-    public function getWeather($city, string $type = 'base', string $format = 'json')
-    {
-        $url = 'https://restapi.amap.com/v3/weather/weatherInfo';
-
+    public function getWeather($city,string $type = 'base', string $format = 'json')
+    {$url = 'https://restapi.amap.com/v3/weather/weatherInfo';
         if (!in_array(strtolower($format), ['xml', 'json'])) {
             throw new InvalidArgumentException('Invalid response format: '.$format);
         }
